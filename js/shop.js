03ac65a0b228d65d5859b2ae66a59f017bf43fb3
JEspinosa1770 <jespinosa1770@gmail.com>
2025-10-19 18:43:55 +0200

    feat(shop): Exercise 2. Function calculateTotal done. Added to buy function to be updated.

--- js/shop.js
+++ js/shop.js
@@ -95,6 +95,7 @@ const buy = (event) => {
         lookingCart.quantity += 1;
     }
     updateQtyTotal();
+    calculateTotal();
 }
 
 // Exercise 2
@@ -106,6 +107,10 @@ const cleanCart = () =>  {
 // Exercise 3
 const calculateTotal = () =>  {
     // Calculate total price of the cart using the "cartList" array
+    const totalAmount = cart.reduce((summary = 0, actualProduct) => {
+        summary += actualProduct.quantity * actualProduct.price;
+        return summary;
+    }, 0)
 }
 
 // Exercise 4
